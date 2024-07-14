8164bea0897fc22c47daffd60077dbcd57230ad5
Shivendra <shivharsh44@gmail.com>
2024-07-14 14:05:42 +0530

    fixing

--- website/backend/index.js
+++ website/backend/index.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 const axios = require('axios');
 const app = express();
 require('dotenv').config({ path: '.env' });
-require('./models/db');
+require('./Models/db');
 
 const authRouter = require('./Routes/AuthRouter');
 const videoRouter = require('./Routes/VideoRouter');
