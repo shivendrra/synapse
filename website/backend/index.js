516167c9fe29c144515b460c7f3ba4673b8ccffc
Shivendra <shivharsh44@gmail.com>
2024-07-17 17:08:56 +0530

    fixed backend for deployment

--- website/backend/index.js
+++ website/backend/index.js
@@ -10,7 +10,9 @@ const axios = require('axios');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const ytdl = require('@distube/ytdl-core');
 const Joi = require('joi');
+const updateAvatarRouter = express.Router();
 require('events').EventEmitter.defaultMaxListeners = 15;
 require('dotenv').config({ path: '.env' });
 
@@ -34,7 +36,24 @@ const UserSchema = new Schema({
   }
 });
 
+const playlistSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  avatarConfig: String,
+  playlists: [{
+    name: String,
+    songs: [{
+      videoId: String,
+      title: String,
+      channel: String,
+      thumbnailUrl: String,
+      description: String,
+    }],
+  }],
+});
+
 const UserModel = mongoose.model('user', UserSchema);
+const playlistModel = mongoose.model('Playlist', playlistSchema);
+
 const app = express();
 const mongo_url = process.env.MONGODB_URL;
 const API_KEY = process.env.yt_key;
@@ -67,6 +86,22 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.use(express.json());
+
+router.post('/update-avatar', async (req, res) => {
+  const { username, avatarConfig } = req.body;
+
+  try {
+    const user = await UserModel.findOneAndUpdate({ username }, { avatarConfig }, { new: true });
+    if (user) {
+      res.json({ success: true, user });
+    } else {
+      res.status(404).json({ success: false, message: 'User not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ success: false, message: 'Internal Server Error' });
+  }
+});
+app.use('/api', updateAvatarRouter);
 
 // Auth Router and Controller
 const authRouter = express.Router();
@@ -134,10 +169,125 @@ const login = async (req, res) => {
   }
 };
 
-authRouter.post('/signup', signupValidation, signup);
+const googleSignup = async (req, res) => {
+  try {
+    const { name, email } = req.body;
+    const user = await UserModel.findOne({ email });
+    if (user) {
+      return res.status(409).json({ message: 'User already exists, try logging in', success: false });
+    }
+    const username = generateFromEmail(email, 4);
+    const userModel = new UserModel({ name, username, email });
+    await userModel.save();
+    res.status(201).json({ message: 'Signed up successfully using Google', success: true });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error', success: false });
+  }
+};
+
+const googleLogin = async (req, res) => {
+  try {
+    const { email } = req.body;
+    const user = await UserModel.findOne({ email });
+    const errorMsg = "User doesn't exist, try signing up";
+    if (!user) {
+      return res.status(403).json({ message: errorMsg, success: false });
+    }
+    const jwtToken = jwt.sign({ email: user.email, _id: user._id }, process.env.JWT_SECRET, { expiresIn: '24h' });
+    res.status(200).json({ message: 'Logged in successfully', success: true, jwtToken, email, name: user.name, username: user.username });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error', success: false });
+  }
+};
+
 authRouter.post('/login', loginValidation, login);
+authRouter.post('/signup', signupValidation, signup);
+authRouter.post('/googleSignup', googleSignup);
+authRouter.post('/googleLogin', googleLogin);
+
+authRouter.get('/login/success', (req, res) => {
+  if (req.user) {
+    res.status(200).json({
+      error: false,
+      message: "Successfully logged in",
+      user: req.user,
+    })
+  } else {
+    res.status(403).json({ error: true, message: "Not Authorized" });
+  }
+});
+
+authRouter.get('/login/failed', (req, res) => {
+  res.status(401).json({
+    error: true,
+    message: 'Login failure'
+  });
+});
+
+authRouter.get('/google/callback',
+  passport.authenticate("google", {
+    successRedirect: 'https://synapse-music.vercel.app',
+    // successRedirect: 'http://localhost:3000/',
+    failureRedirect: '/login/failed',
+  })
+);
+
+authRouter.get("/google", passport.authenticate("google", ["profile", "email"]));
+authRouter.get("/logout", (req, res) => {
+  req.logout();
+  // res.redirect('http://localhost:3000');
+  res.redirect('https://synapse-music.vercel.app');
+});
 
 app.use('/auth', authRouter);
+
+const playlistRouter = express.Router();
+
+playlistRouter.post('/add-playlist', async (req, res) => {
+  const { username, playlistName, song } = req.body;
+
+  try {
+    let user = await playlistModel.findOne({ username });
+
+    if (!user) {
+      user = new playlistModel({ username, playlists: [] });
+    }
+
+    const playlist = user.playlists.find(p => p.name === playlistName);
+    if (playlist) {
+      playlist.songs.push(song);
+    } else {
+      user.playlists.push({ name: playlistName, songs: [song] });
+    }
+
+    await user.save();
+    res.json({ success: true, user });
+
+  } catch (error) {
+    res.status(500).json({ success: false, message: 'Internal Server Error', error });
+  }
+});
+
+playlistRouter.get('/get-playlists/:username', async (req, res) => {
+  const { username } = req.params;
+
+  try {
+    const user = await playlistModel.findOne({ username });
+
+    if (user) {
+      res.json({ success: true, playlists: user.playlists });
+    } else {
+      res.status(404).json({ success: false, message: 'User not found' });
+    }
+
+  } catch (error) {
+    res.status(500).json({ success: false, message: 'Internal Server Error', error });
+  }
+});
+
+app.use('/playlists', playlistRouter);
 
 // Video Router and Controller
 const videoRouter = express.Router();
@@ -216,6 +366,64 @@ app.post('/search', async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).send('Error fetching videos');
+  }
+});
+
+app.get('/download', async (req, res) => {
+  try {
+    const videoId = req.query.id;
+    const videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
+    if (!ytdl.validateURL(videoUrl)) {
+      return res.status(400).json({ error: 'Invalid URL' });
+    }
+
+    const videoInfo = await ytdl.getInfo(videoUrl);
+    const videoTitle = videoInfo.videoDetails.title;
+    const sanitizedTitle = videoTitle.replace(/[^a-z0-9]/gi, '_').toLowerCase();
+    const downloadDir = path.resolve(__dirname, 'downloads');
+    if (!fs.existsSync(downloadDir)) {
+      fs.mkdirSync(downloadDir);
+    }
+    const filePath = path.resolve(downloadDir, `${sanitizedTitle}.mp3`);
+
+    const audioStream = ytdl(videoUrl, {
+      filter: 'audioonly',
+      requestOptions: {
+        headers: {
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        },
+      },
+    });
+    const output = fs.createWriteStream(filePath);
+
+    audioStream.pipe(output);
+
+    output.on('finish', () => {
+      res.download(filePath, `${sanitizedTitle}.mp3`, (err) => {
+        if (err) {
+          console.error('Error sending file:', err);
+          res.status(500).send('Error sending file');
+        } else {
+          fs.unlinkSync(filePath);
+        }
+      });
+    });
+
+    req.on('close', () => {
+      if (!res.headersSent) {
+        res.status(499).send('Client closed request');
+      }
+      audioStream.destroy();
+      output.end();
+    });
+
+    audioStream.on('error', (err) => {
+      console.error('Error downloading video:', err);
+      res.status(500).send('Error downloading video');
+    });
+  } catch (error) {
+    console.error('Error:', error.message);
+    res.status(500).send('Internal Server Error');
   }
 });
 
