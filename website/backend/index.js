f355166d93cb310e614efbe07f5e3264916ed674
Shivendra <shivharsh44@gmail.com>
2024-07-25 19:13:11 +0530

    more changes

--- website/backend/index.js
+++ website/backend/index.js
@@ -20,15 +20,12 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
   name: {
     type: String,
-    required: true,
   },
   username: {
     type: String,
-    required: true,
   },
   email: {
     type: String,
-    required: true,
   },
   password: {
     type: String,
@@ -78,10 +75,10 @@ const authRouter = express.Router();
 
 const signupValidation = (req, res, next) => {
   const schema = Joi.object({
-    name: Joi.string().min(3).max(100).required(),
-    email: Joi.string().email().required(),
-    username: Joi.string().min(3).max(15).required(),
-    password: Joi.string().min(4).max(20).required(),
+    name: Joi.string().min(3).max(100),
+    email: Joi.string().email(),
+    username: Joi.string().min(3).max(15),
+    password: Joi.string().min(4).max(20),
   });
   const { error } = schema.validate(req.body);
   if (error) {
@@ -92,8 +89,8 @@ const signupValidation = (req, res, next) => {
 
 const loginValidation = (req, res, next) => {
   const schema = Joi.object({
-    email: Joi.string().email().required(),
-    password: Joi.string().min(4).max(20).required(),
+    email: Joi.string().email(),
+    password: Joi.string().min(4).max(20),
   });
   const { error } = schema.validate(req.body);
   if (error) {
